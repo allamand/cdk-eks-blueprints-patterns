60612fe209fcf3b6a3f37d4dd073eae42ce0bc2a
Sebastien Allamand <sallaman@amazon.com>
2022-03-23 13:23:12 +0000

    readd karpenter

--- lib/pipeline-stack/index.ts
+++ lib/pipeline-stack/index.ts
@@ -124,10 +124,10 @@ export default class PipelineConstruct {
         new ssp.CalicoAddOn(),
         new ssp.MetricsServerAddOn(),
         //new ssp.ClusterAutoScalerAddOn(),
-        // new ssp.addons.KarpenterAddOn({
-        //   version: 'v0.7.0',
-        //   ProvisionerSpecs: provisionerSpecs,
-        // }),
+        new ssp.addons.KarpenterAddOn({
+          //version: 'v0.7.0',
+          ProvisionerSpecs: provisionerSpecs,
+        }),
         new ssp.ContainerInsightsAddOn(),
         new ssp.XrayAddOn(),
         new ssp.SecretsStoreAddOn(),
